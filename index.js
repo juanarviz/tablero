e216d058f731435432c4bae1595271d38d408461
Hellen Mendoza <hellenmendoza08@gmail.com>
2019-07-27 11:45:58 -0600

    subir

--- index.js
+++ index.js
@@ -79,82 +79,26 @@ var Game = new Phaser.Class({
         this.text;
     },
 
+    preload: function()
+    {
+      this.load.image('tabla', 'assets/tabla.png');
+    },
+
     create: function ()
     {
         console.log('%c Game ', 'background: green; color: white; display: block;');
-
-        this.physics.world.setBounds(0, 0, 800 * 2, 600 * 2);
-
-        var spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -100, -100);
-
-        //  Create loads of random sprites
-
-        var anims = [ 'diamond', 'prism', 'ruby', 'square' ];
-
-        for (var i = 0; i < 50; i++)
-        {
-            var pos = Phaser.Geom.Rectangle.Random(spriteBounds);
-
-            var block = this.physics.add.sprite(pos.x, pos.y, 'gems');
-
-            block.setVelocity(Phaser.Math.Between(200, 400), Phaser.Math.Between(200, 400));
-            block.setBounce(1).setCollideWorldBounds(true);
-
-            if (Math.random() > 0.5)
-            {
-                block.body.velocity.x *= -1;
-            }
-            else
-            {
-                block.body.velocity.y *= -1;
-            }
-
-            block.play(Phaser.Math.RND.pick(anims));
-
-            if (i === 0)
-            {
-                this.track = block;
-            }
-        }
-
-        var cursors = this.input.keyboard.createCursorKeys();
-
-        var controlConfig = {
-            camera: this.cameras.main,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
-            zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
-            acceleration: 0.06,
-            drag: 0.0005,
-            maxSpeed: 1.0
-        };
-
-        this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-
-        this.add.text(0, 0, 'Use Cursors to scroll camera.\nClick to Quit', { font: '18px Courier', fill: '#00ff00' }).setScrollFactor(0);
-
-        this.text = this.add.text(400, 0, '', { font: '16px Courier', fill: '#00ff00' });
-
-        this.input.once('pointerup', function () {
-
-            this.scene.start('gameover');
-
-        }, this);
-
+        this.add.image(400, 300, 'tabla');
     },
 
-    update: function (time, delta)
-    {
-        this.controls.update(delta);
-
-        this.text.setText([
-            'x: ' + this.track.x,
-            'y: ' + this.track.y
-        ]);
-    }
+    // update: function (time, delta)
+    // {
+    //     this.controls.update(delta);
+    //
+    //     this.text.setText([
+    //         'x: ' + this.track.x,
+    //         'y: ' + this.track.y
+    //     ]);
+    // }
 
 });
 
